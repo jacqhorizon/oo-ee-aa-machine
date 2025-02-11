8d701a0a41d1bfbe7fd4df388257c0623eebb966
Jacq <jacqkirkman@gmail.com>
2025-02-11 10:12:20 -0800

    update config to resolve images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/oo-ee-aa-machine',
+    output: 'export',
+    reactStrictMode: true
+};
 
 export default nextConfig;
